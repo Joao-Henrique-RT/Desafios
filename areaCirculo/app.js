1b00eebf0f210fd86e2360df1cf6002cf319046d
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2024-04-09 23:12:52 -0300

    atualizações

---
+++ areaCirculo/app.js
@@ -0,0 +1,13 @@
+
+    function Calcular(){
+        var n = 3.14159
+        var raio
+        var area
+        raio = document.getElementById("raio").value
+
+        area = n * raio**2
+
+        alert(area)
+    }
+
+
